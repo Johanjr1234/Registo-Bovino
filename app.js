3de511355f5e89f39b622ddcfe908859cd4c47ba
Johanjr1234 <johanroncancio16z@gmail.com>
2025-11-25 19:46:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Archivo: app.js
+// Archivo: app.js 
 import { db, storage } from './firebase-config.js';
 import { collection, addDoc, getDocs, serverTimestamp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 import { ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js';
