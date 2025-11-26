b729c778453553820204f018d0d760765061dea5
Johanjr1234 <johanroncancio16z@gmail.com>
2025-11-26 07:56:38 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,17 @@
 // Archivo: firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-// Tu configuración (Copiada de tu captura)
+// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBIRieWbycyjG2wEYjXXj4SvwOo2Af3Ubc",
+  // ... ( // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIRieWbycyJg2wEYjXXj4SvwOo2Af3Ubc",
   authDomain: "ganaderia-control-3dd11.firebaseapp.com",
   projectId: "ganaderia-control-3dd11",
   storageBucket: "ganaderia-control-3dd11.firebasestorage.app",
@@ -13,9 +19,17 @@ const firebaseConfig = {
   appId: "1:316391024184:web:17dc1fe0fd7dfc100f28cd"
 };
 
-// Inicializar Firebase (SOLO UNA VEZ)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);)
+  // ...
+};
+
+// **NUEVA LLAVE AGREGADA PARA IMGBB**
+const IMGBB_API_KEY = "76b0f8378bb6ee12aae17f2bf7b14879"; 
+
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { db, storage };
+// Exportamos la base de datos y la llave de ImgBB
+export { db, IMGBB_API_KEY };
