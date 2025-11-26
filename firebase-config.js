755bb6de831056925383ecea6bdc508ab33d65ca
Johanjr1234 <johanroncancio16z@gmail.com>
2025-11-26 06:41:13 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,14 +3,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Tu configuración (Copiada de tu captura)
 const firebaseConfig = {
-  apiKey: "AIzaSyBIRieWbycyJg2wEYjXXj4SvwOo2Af3Ubc",
+  apiKey: "AIzaSyBIRieWbycyjG2wEYjXXj4SvwOo2Af3Ubc",
   authDomain: "ganaderia-control-3dd11.firebaseapp.com",
   projectId: "ganaderia-control-3dd11",
   storageBucket: "ganaderia-control-3dd11.firebasestorage.app",
@@ -18,9 +13,7 @@ const firebaseConfig = {
   appId: "1:316391024184:web:17dc1fe0fd7dfc100f28cd"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Inicializar Firebase
+// Inicializar Firebase (SOLO UNA VEZ)
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
